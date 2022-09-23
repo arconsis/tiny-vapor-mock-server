1254b9df2b990a58061d32197b22ec71cffde9e1
Moritz Ellerbrock <github@elmoritz.eu>
2022-09-23 16:55:42 +0200

    Twitch-Stream 23.09.22

--- Sources/App/Model/FilenameObject.swift
+++ Sources/App/Model/FilenameObject.swift
@@ -1,4 +1,5 @@
 import Vapor
+import RoutingKit
 
 struct FilenameObject {
     let path: String
@@ -10,12 +11,75 @@ struct FilenameObject {
     }
     
     var sourcePath: String {
+        
         // path = "/" -> "index.json"
         // path
+        // path/to/amount/10/page/1/file.json
+        let sourcePath = "\(filePath)\(fileName)"
+        print("###: \(sourcePath)")
+        return sourcePath
+    }
+    
+    private var fileName: String {
+        let filename = path.pathComponents.last ?? "index"
+        return "\(filename).json"
+    }
+    
+    private var filePath: String {
+        return "\(pathComponentFilePath)\(queryFilePath)"
+    }
+    
+    private var pathComponentFilePath: String {
         guard !path.pathComponents.isEmpty else {
-            return "index.json"
+            return ""
         }
         
-        return "\(String(path.dropFirst(1))).json"
+        let pathComponents = path.pathComponents.dropLast()
+        let stringComponents = pathComponents.map{ String($0.description) }
+        
+        var pathPart = stringComponents.joined(separator: "/")
+        if pathPart.isEmpty {
+            return ""
+        } else {
+            return "\(pathPart)/"
+        }
+    }
+    
+    private var queryFilePath: String {
+        guard let query = query, !query.isEmpty else {
+            return ""
+        }
+        let dict = query.toDictionary()
+        var queryPath = ""
+        dict.keys
+            .sorted()
+            .forEach{ key in
+            if let value = dict[key] {
+                queryPath.append("\(key)/\(value)/")
+            }
+        }
+        
+        return queryPath
+    }
+}
+
+extension String {
+    func toDictionary() -> Dictionary<String, String> {
+        var dictionary: Dictionary<String, String> = .init()
+        
+        self.split(separator: "&")
+            .map{ String($0) }
+            .forEach{
+                let keyValue = $0.split(separator: "=").map{ String($0) }
+                guard
+                    keyValue.count == 2,
+                    let key = keyValue.first,
+                    let value = keyValue.last
+                else { return }
+                      
+                dictionary[key] = value
+            }
+        
+        return dictionary
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,14 +3,22 @@ import Vapor
 func routes(_ app: Application) throws {
     app.get("**", use: captchaAll(req:))
     app.get(use: captchaAll(req:))
+    
 }
 
 func captchaAll(req: Request) async throws -> String {
     let filename = FilenameObject(with: req.url)
-    let byteBuffer = try await req.fileio.collectFile(at: filename.sourcePath)
-    return String(buffer: byteBuffer)
+    do {
+        let byteBuffer = try await req.fileio.collectFile(at: filename.sourcePath)
+        return String(buffer: byteBuffer)
+    } catch {
+        print(error)
+        return filename.sourcePath
+    }
 }
 
+// http://127.0.0.1/ -> /index.json => DONE!!!!
+// http://127.0.0.1/index -> /index.json => DONE!!!!
 // http://127.0.0.1/path/to/a/file -> /path/to/a/file.json => DONE!!!!
 // http://127.0.0.1/path/to/a/file?amount=10&page=1 -> /path/to/a/file/amount=10.json
 // http://127.0.0.1/path/to/a/file?page=1&amount=10 -> /path/to/a/file/amount=10.json
